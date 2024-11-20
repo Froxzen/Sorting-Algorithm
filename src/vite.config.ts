c86d319bc15b53930212c7e97cd8cd4d69ef5549
Froxzen <harvardchong1@gmail.com>
2024-11-20 22:48:44 +1100

    Audited packages

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	root: ".",
 	base: "/Sorting-Algorithm/",
-	server: {
-		port: 3000,
-		open: true,
-	},
 	build: {
-		outDir: "dist",
-		sourcemap: true,
+		outDir: "dist"
 	},
 });
