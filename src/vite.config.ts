06bb83032f64de3cfb020c23a0942e6ea88d9f66
Froxzen <harvardchong1@gmail.com>
2024-11-20 22:27:15 +1100

    Fixed Github Pages site

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    root: '.',
-    base: '/',
-    server: {
-        port: 3000,
-        open: true
-    },
-    build: {
-        outDir: 'dist',
-        sourcemap: true
-    }
+	root: ".",
+	base: "/Sorting-Algorithm/",
+	server: {
+		port: 3000,
+		open: true,
+	},
+	build: {
+		outDir: "dist",
+		sourcemap: true,
+	},
 });
